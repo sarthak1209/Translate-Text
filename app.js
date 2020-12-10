1e2d7fafe0040f901475ea7a8c7a13455b9dda39
Sarthak Lav <sarthakchaudhary52@gmail.com>
2020-12-10 14:01:55 +0530

    final

--- app.js
+++ app.js
@@ -1,38 +1,43 @@
 var inputText = document.querySelector("#txt-input");
 console.log(inputText);
 var translateButton = document.querySelector("#btn-translate");
-var outputText    = document.querySelector("#output-section");
+var outputText = document.querySelector("#output-section");
 
 
 var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
 
 
-function constructUrl(input){
+function constructUrl(input) {
     var url = serverUrl + "?" + "text=" + input;
     //console.log(url);
     return url;
 }
- function errorHandeler(error){
-     console.log("An error occured", error);
-     alert("Server is not responding!!!, Please try again after some time..")
- }
+
+function errorHandeler(error) {
+    console.log("An error occured", error);
+    alert("Server is not responding!!!, Please try again after some time..")
+}
 
 
 
-function clickHandeller(){
+function clickHandeller() {
     var input = inputText.value;
-    console.log(input);
+    //sconsole.log(input);
 
-    
-    fetch(constructUrl(input))
-    .then(res => res.json())
-    .then(json => {
-        let displayText =  json.contents.translated; 
+    if (input.length != 0) {
+        fetch(constructUrl(input))
+            .then(res => res.json())
+            .then(json => {
+                let displayText = json.contents.translated;
 
-        outputText.innerText = displayText;
-    }).catch(errorHandeler);
+                outputText.innerText = displayText;
+            }).catch(errorHandeler);
+    } else {
+        alert("Please type some text to translate....")
+    }
+
+
 }
 
 
 translateButton.addEventListener("click", clickHandeller);
-
